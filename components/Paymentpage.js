ed2a28f341dbbb5e9cc958c508bcce76f1ef9dd3
Udaykumar-07 <97972727+Udaykumar-07@users.noreply.github.com>
2025-01-31 12:47:20 +0000

    .

--- components/Paymentpage.js
+++ components/Paymentpage.js
@@ -41,7 +41,7 @@ const Paymentpage = ({username}) => {
           transition: Bounce,
           });
           }
-        router.push(`/${username}`)
+        router.push(/${username})
         }, [searchParams, router, username])
         
         const handleChange = (e)=>{
@@ -66,8 +66,8 @@ const Paymentpage = ({username}) => {
             "name": "get me a chai", //your business name
             "description": "Test Transaction",
             "image": "https://example.com/your_logo",
-            "order_id": orderId, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-            "callback_url": `${process.env.NEXT_PUBLIC_URL}/api/razorpay`,
+            "order_id": orderId, //This is a sample Order ID. Pass the id obtained in the response of Step 1
+            "callback_url": ${process.env.NEXT_PUBLIC_URL}/api/razorpay,
             "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
                 "name": currentUser.name || "", //your customer's name
                 "email": currentUser.email || "",
