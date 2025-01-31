8a029d6e7de79174ed778347d4465e30e7dbfbaa
Udaykumar-07 <97972727+Udaykumar-07@users.noreply.github.com>
2025-01-31 15:22:04 +0000

    .

--- components/Navbar.js
+++ components/Navbar.js
@@ -51,7 +51,7 @@ const Navbar = () => {
                >Logout</button>
                </>
                )}
-         {!session && (<Link onClick={() => signOut()} href="/Login">
+         {!session && (<Link onClick={() => signOut()} href="#">
          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>)}
          
         
