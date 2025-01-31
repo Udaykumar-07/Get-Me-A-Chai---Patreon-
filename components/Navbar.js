f2d80d49f717664a57261df20dacdc7db9899c3e
Udaykumar-07 <97972727+Udaykumar-07@users.noreply.github.com>
2025-01-31 15:33:41 +0000

    .

--- components/Navbar.js
+++ components/Navbar.js
@@ -47,11 +47,11 @@ const Navbar = () => {
     </ul>
 </div>
 </div>
-            <button className='text-white w-fit bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 ' onClick={()=>  signOut()}
-               >Logout</button>
+          <Link onClick={()=>  signOut()} href="#">
+          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>
                </>
                )}
-         {!session && (<Link onClick={() => signOut()} href="#">
+         {!session && (<Link href="/Login">
          <button className='text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2 '>Login</button></Link>)}
          
         
